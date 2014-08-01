5cc3a838a56687a18cb39e175873ca345fd9d430
Jayjay <gvcayetano@yahoo.com>
2014-08-01 09:32:54 +0800

    Added user scaffold and sign in / up anchor
    Added User scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140721021606) do
+ActiveRecord::Schema.define(version: 20140801012800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20140721021606) do
   create_table "mercs", force: true do |t|
     t.string   "name"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
